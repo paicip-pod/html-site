9af2c2255f5b82f8287702d8735229156587a834
Preshailen Ruthnam <ruthnampresh@gmail.com>
2023-06-01 22:30:31 +0200

    changed copytoclipboard to a promise

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
 function copyToClipboard() {
-    navigator.clipboard.writeText('https://media.rss.com/paicip-pod/feed.xml');
-    Swal.fire({
-        title: 'Success!',
-        text: 'RSS Feed successfully copied to clipboard',
-        icon: 'success',
-        timer: 1800,
-        timerProgressBar: true,
-        showCancelButton: false,
-        showCloseButton: false,
-        showConfirmButton: false
+    navigator.clipboard.writeText('https://media.rss.com/paicip-pod/feed.xml').then(f => {
+        Swal.fire({
+            title: 'Success!',
+            text: 'RSS Feed successfully copied to clipboard',
+            icon: 'success',
+            timer: 1800,
+            timerProgressBar: true,
+            showCancelButton: false,
+            showCloseButton: false,
+            showConfirmButton: false
+        });
+    }).catch(err => {
+        Swal.fire({
+            title: 'Failure!',
+            text: 'Error copying RSS Feed to clipboard',
+            icon: 'error',
+            timer: 1800,
+            timerProgressBar: true,
+            showCancelButton: false,
+            showCloseButton: false,
+            showConfirmButton: false
+        });
     });
 }
