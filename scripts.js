55e3f07c2f00b3dfe705616bd87700d4a19a6c75
Preshailen Ruthnam <ruthnampresh@gmail.com>
2023-06-01 22:43:23 +0200

    ddg

--- scripts.js
+++ scripts.js
@@ -1,27 +1,31 @@
 function copyToClipboard() {
-    navigator.clipboard.writeText('https://media.rss.com/paicip-pod/feed.xml').then(f => {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText('https://media.rss.com/paicip-pod/feed.xml').then(f => {
         Swal.fire({
-            title: 'Success!',
-            text: 'RSS Feed successfully copied to clipboard',
-            icon: 'success',
-            timer: 1800,
-            timerProgressBar: true,
-            showCancelButton: false,
-            showCloseButton: false,
-            showConfirmButton: false
+          title: 'Success!',
+          text: 'RSS Feed successfully copied to clipboard',
+          icon: 'success',
+          timer: 1800,
+          timerProgressBar: true,
+          showCancelButton: false,
+          showCloseButton: false,
+          showConfirmButton: false
         });
-    }).catch(err => {
-        
+      }).catch(err => {
         Swal.fire({
-            title: 'Failure!',
-            text: err,
-            //text: 'Error copying RSS Feed to clipboard',
-            icon: 'error',
-            timer: 1800,
-            timerProgressBar: true,
-            showCancelButton: false,
-            showCloseButton: false,
-            showConfirmButton: false
+          title: 'Failure!',
+          text: 'Error copying RSS Feed to clipboard',
+          icon: 'error',
+          timer: 1800,
+          timerProgressBar: true,
+          showCancelButton: false,
+          showCloseButton: false,
+          showConfirmButton: false
         });
-    });
+      });
+    } else {
+        Swal.fire({
+            text: 'here'
+        })
+    }
 }
