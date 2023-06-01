eb2c4164a764b0c0d241713c121db0305e3af665
Preshailen Ruthnam <ruthnampresh@gmail.com>
2023-06-01 23:50:11 +0200

    fix

--- scripts.js
+++ scripts.js
@@ -1,50 +1,51 @@
 function copyToClipboard() {
-    if (!!navigator.clipboard) {
-      navigator.clipboard.writeText('https://media.rss.com/paicip-pod/feed.xml').then(f => {
-        Swal.fire({
-          title: 'Success!',
-          text: 'navigator',
-          icon: 'success',
-          timer: 1800,
-          timerProgressBar: true,
-          showCancelButton: false,
-          showCloseButton: false,
-          showConfirmButton: false
-        });
-      }).catch(err => {
-        Swal.fire({
-          title: 'Failure!',
-          text: 'this error', //'Error copying RSS Feed to clipboard',
-          icon: 'error',
-          timer: 1800,
-          timerProgressBar: true,
-          showCancelButton: false,
-          showCloseButton: false,
-          showConfirmButton: false
-        });
-      });
-    } else {
-        var textarea = document.createElement("textarea");
-        textarea.textContent = 'https://media.rss.com/paicip-pod/feed.xml';
-        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
-        document.body.appendChild(textarea);
-        textarea.select();
-        try {
-            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
-        }
-        catch (ex) {
+    navigator.permissions.query({name: "clipboard-write"}).then((result) => {
+        if (result.state === 'granted') {
+          navigator.clipboard.writeText('https://media.rss.com/paicip-pod/feed.xml').then(f => {
             Swal.fire({
-                title: 'Failure!',
-                text: 'that errpr',
-                icon: 'error',
-                timer: 1800,
-                timerProgressBar: true,
-                showCancelButton: false,
-                showCloseButton: false,
-                showConfirmButton: false
+              title: 'Success!',
+              text: 'navigator',
+              icon: 'success',
+              timer: 1800,
+              timerProgressBar: true,
+              showCancelButton: false,
+              showCloseButton: false,
+              showConfirmButton: false
             });
-        }
-        finally {
+          }).catch(err => {
+            Swal.fire({
+              title: 'Failure!',
+              text: 'Error copying RSS Feed to clipboard',
+              icon: 'error',
+              timer: 1800,
+              timerProgressBar: true,
+              showCancelButton: false,
+              showCloseButton: false,
+              showConfirmButton: false
+            });
+          });
+        } else {
+          var textarea = document.createElement("textarea");
+          textarea.textContent = 'https://media.rss.com/paicip-pod/feed.xml';
+          textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+          document.body.appendChild(textarea);
+          textarea.select();
+          try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+          }
+          catch (ex) {
+            Swal.fire({
+              title: 'Failure!',
+              text: 'that errpr',
+              icon: 'error',
+              timer: 1800,
+              timerProgressBar: true,
+              showCancelButton: false,
+              showCloseButton: false,
+              showConfirmButton: false
+            });
+          }
+          finally {
             Swal.fire({
                 title: 'Success!',
                 text: 'finally',
@@ -56,6 +57,7 @@ function copyToClipboard() {
                 showConfirmButton: false
               });
             document.body.removeChild(textarea);
+          }
         }
-    }
+    }).catch(err => err);
 }
