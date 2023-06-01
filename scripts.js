70e41e3bf986023b210a00b9d991be60cc8491a7
Preshailen Ruthnam <ruthnampresh@gmail.com>
2023-06-01 22:55:33 +0200

    workaround

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function copyToClipboard() {
-    if (document.navigator.clipboard) {
+    if (false) {
         console.log('there');
       navigator.clipboard.writeText('https://media.rss.com/paicip-pod/feed.xml').then(f => {
         Swal.fire({
@@ -25,9 +25,20 @@ function copyToClipboard() {
         });
       });
     } else {
-        console.log('h000ere');
-        Swal.fire({
-            text: 'here'
-        })
+        var textarea = document.createElement("textarea");
+        textarea.textContent = 'https://media.rss.com/paicip-pod/feed.xml';
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        }
+        catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return prompt("Copy to clipboard: Ctrl+C, Enter", text);
+        }
+        finally {
+            document.body.removeChild(textarea);
+        }
     }
 }
