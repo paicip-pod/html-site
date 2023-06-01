57f90ed827e6bb9abce9c5fc46e26d83ff544870
Preshailen Ruthnam <ruthnampresh@gmail.com>
2023-06-02 00:11:16 +0200

    check

--- scripts.js
+++ scripts.js
@@ -1,87 +1,90 @@
 function copyToClipboard() {
-    navigator.permissions.query({name: "clipboard-write"}).then((result) => {
-        Swal.fire({
-            title: 'Success!',
-            text: 'navigator',
-            icon: 'success',
-            timer: 1800,
-            timerProgressBar: true,
-            showCancelButton: false,
-            showCloseButton: false,
-            showConfirmButton: false
-          });
-        if (result.state === 'granted') {
-          navigator.clipboard.writeText('https://media.rss.com/paicip-pod/feed.xml').then(f => {
-            Swal.fire({
-              title: 'Success!',
-              text: 'navigator',
-              icon: 'success',
-              timer: 1800,
-              timerProgressBar: true,
-              showCancelButton: false,
-              showCloseButton: false,
-              showConfirmButton: false
-            });
-          }).catch(err => {
-            Swal.fire({
-              title: 'Failure!',
-              text: 'Error copying RSS Feed to clipboard',
-              icon: 'error',
-              timer: 1800,
-              timerProgressBar: true,
-              showCancelButton: false,
-              showCloseButton: false,
-              showConfirmButton: false
-            });
-          });
-        } else {
-          try {
-            var textarea = document.createElement("textarea");
-            textarea.textContent = 'https://media.rss.com/paicip-pod/feed.xml';
-            textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
-            document.body.appendChild(textarea);
-            textarea.select();
-            let result = document.execCommand("copy");  // Security exception may be thrown by some browsers.
-            document.body.removeChild(textarea);
-            if (result) {
+    if (navigator) {
+        navigator.permissions.query({name: "clipboard-write"}).then((result) => {
+            if (result.state === 'granted') {
+              navigator.clipboard.writeText('https://media.rss.com/paicip-pod/feed.xml').then(f => {
                 Swal.fire({
-                    title: 'Success!',
-                    text: 'finally',
-                    icon: 'success',
-                    timer: 1800,
-                    timerProgressBar: true,
-                    showCancelButton: false,
-                    showCloseButton: false,
-                    showConfirmButton: false
-                  });
-              
+                  title: 'Success!',
+                  text: 'navigator',
+                  icon: 'success',
+                  timer: 1800,
+                  timerProgressBar: true,
+                  showCancelButton: false,
+                  showCloseButton: false,
+                  showConfirmButton: false
+                });
+              }).catch(err => {
+                Swal.fire({
+                  title: 'Failure!',
+                  text: 'Error copying RSS Feed to clipboard',
+                  icon: 'error',
+                  timer: 1800,
+                  timerProgressBar: true,
+                  showCancelButton: false,
+                  showCloseButton: false,
+                  showConfirmButton: false
+                });
+              });
             } else {
+              try {
+                var textarea = document.createElement("textarea");
+                textarea.textContent = 'https://media.rss.com/paicip-pod/feed.xml';
+                textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+                document.body.appendChild(textarea);
+                textarea.select();
+                let result = document.execCommand("copy");  // Security exception may be thrown by some browsers.
+                document.body.removeChild(textarea);
+                if (result) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'finally',
+                        icon: 'success',
+                        timer: 1800,
+                        timerProgressBar: true,
+                        showCancelButton: false,
+                        showCloseButton: false,
+                        showConfirmButton: false
+                      });
+                  
+                } else {
+                    Swal.fire({
+                        title: 'Failure!',
+                        text: 'else',
+                        icon: 'error',
+                        timer: 1800,
+                        timerProgressBar: true,
+                        showCancelButton: false,
+                        showCloseButton: false,
+                        showConfirmButton: false
+                      });
+                }
+              }
+              catch (ex) {
                 Swal.fire({
-                    title: 'Failure!',
-                    text: 'else',
-                    icon: 'error',
-                    timer: 1800,
-                    timerProgressBar: true,
-                    showCancelButton: false,
-                    showCloseButton: false,
-                    showConfirmButton: false
-                  });
+                  title: 'Failure!',
+                  text: 'that errpr',
+                  icon: 'error',
+                  timer: 1800,
+                  timerProgressBar: true,
+                  showCancelButton: false,
+                  showCloseButton: false,
+                  showConfirmButton: false
+                });
+              }
             }
-          }
-          catch (ex) {
+        }).catch(err => {
             Swal.fire({
-              title: 'Failure!',
-              text: 'that errpr',
-              icon: 'error',
-              timer: 1800,
-              timerProgressBar: true,
-              showCancelButton: false,
-              showCloseButton: false,
-              showConfirmButton: false
-            });
-          }
-        }
-    }).catch(err => {
+                title: 'Failure!',
+                text: 'catch permission',
+                icon: 'error',
+                timer: 1800,
+                timerProgressBar: true,
+                showCancelButton: false,
+                showCloseButton: false,
+                showConfirmButton: false
+              });
+        });
+    } else {
         Swal.fire({
             title: 'Failure!',
             text: 'catch permission',
@@ -92,5 +95,6 @@ function copyToClipboard() {
             showCloseButton: false,
             showConfirmButton: false
           });
-    });
+    }
+    
 }
